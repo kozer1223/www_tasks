83420832a5bdeb4c022dcecf6752400699920050
kozer1223 <kozerskilo@interia.pl>
2018-04-16 16:18:09 +0200

    Replace clear button with copy button

--- frame/js/replace.js
+++ frame/js/replace.js
@@ -1,38 +1,33 @@
-function replaceLetters() {
-  "use strict";
-  var countArea = document.getElementById("replace-area");
-  var origStr = countArea.value;
-
-  var newStr = origStr.replace(/[&<>"'\/]/g, function(x){
-    switch(x){
-      case "&": return "&amp;"; break;
-      case "<": return "&lt;"; break;
-      case ">": return "&gt;"; break;
-      case "\"": return "&quot;"; break;
-      case "'": return "&#x27;"; break;
-      case "/": return "&#x2F;"; break;
-      default: return x;
-    };
-  });
-
-  countArea.value = newStr;
-}
-
-function clearTextArea() {
-  "use strict";
-  var countArea = document.getElementById("replace-area");
-  countArea.value = "";
-}
-
-function setUpButtons(replace, clear) {
+function setUpButtons(replace, copy, textArea) {
   "use strict";
   var replaceButton = document.getElementById(replace);
-  var clearButton = document.getElementById(clear);
-  replaceButton.onclick = replaceLetters;
-  clearButton.onclick = clearTextArea;
+  var copyButton = document.getElementById(copy);
+  var countArea = document.getElementById(textArea);
+
+  replaceButton.onclick = function() {
+    "use strict";
+    var origStr = countArea.value;
+    countArea.value = origStr.replace(/[&<>"'\/]/g, function(x){
+      switch(x){
+        case "&": return "&amp;"; break;
+        case "<": return "&lt;"; break;
+        case ">": return "&gt;"; break;
+        case "\"": return "&quot;"; break;
+        case "'": return "&#x27;"; break;
+        case "/": return "&#x2F;"; break;
+        default: return x;
+      };
+    });
+  }
+
+  copyButton.onclick = function() {
+    "use strict";
+    countArea.select();
+    document.execCommand("Copy");
+  }
 }
 
 window.onload = function () {
   "use strict";
-  setUpButtons("btn-replace", "btn-clear");
+  setUpButtons("btn-replace", "btn-copy", "replace-area");
 }
